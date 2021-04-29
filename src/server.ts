aaf2d8cf2c426be241bbd918f240de0ce1f40822
Lucas Mota <lucas.o.mota@gmail.com>
2021-04-28 22:19:31 -0300

    feat(swaggerUi config): Configuração inicial do swaggerUi para documentação da api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
-import { categoriesRoutes } from './routes/categories.routes';
 import { router } from './routes/index';
-import { specificationRoutes } from './routes/specification.routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
